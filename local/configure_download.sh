0b630ab9511d2f3d6f4afaf1b77824e723b818e3
OlliSagi <ollisagittarius@gmail.com>
2023-07-18 16:26:24 +0200

    Update configure_download.sh
    update download

--- local/configure_download.sh
+++ local/configure_download.sh
@@ -1,46 +1,37 @@
-#!/bin/bash
+#!/bin/sh
 (return 0 2>/dev/null) && sourced=1 || sourced=0
 if [ $sourced -eq 0 ]; then
     echo "this script should not be run directly but through configure.sh in the kaldi_nl root directory">&2
 fi
 
 #Set model directory
-if [ -d models ] && [ -z "$modelpack" ]; then
+if [ "$NODOWNLOAD" = "1" ]; then
+    [ -z "$modelpack" ] && fatalerror "\$modelpack must be set if \$NODOWNLOAD is set"
+elif [ -d models ] && [ -z "$modelpack" ]; then
     modelpack=$(realpath models)
 elif [ ! -d models/NL ] && [ -z "$modelpack" ]; then
-    while [ $return_value -eq 0 ] && ! readlink -f $modelpack; do
+    while [ "$return_value" -eq 0 ] && ! readlink -f "$modelpack"; do
         modelpack=$(dialog --stdout --title "Models not found" --inputbox "Enter location to download & store models, do not use ~ " 0 0 "models")
         return_value=$?
     done
-    [ ! $return_value -eq 0 ] && fatalerror "Models not downloaded. Cancelling"
+    [ ! "$return_value" -eq 0 ] && fatalerror "Models not downloaded. Cancelling"
 fi
 [ -z "$modelpack" ] && fatalerror "Model base directory is empty"
 mkdir -p "$modelpack" || fatalerror "Model base directory $modelpack does not exist and unable to create"
-modelpack=$(realpath $modelpack)
+modelpack="$(realpath "$modelpack")"
 #link to the model pack directory from the kaldi_nl root
 if [ ! -e models ]; then
-    ln -s -f $modelpack models
+    ln -s -f "$modelpack" models
 fi
 
 #we need this very ugly patch,
 #creating a symlink back to itself,
 #otherwise certain models break
-if [ ! -e models/Models ]; then
-    ln -s $(realpath models) models/Models
+if [ ! -e models/Models ] && [ "$NODOWNLOAD" != "1" ]; then
+    ln -s "$(realpath models)" models/Models
 fi
 
 # get models
-#
-if [ ! -d models/NL ]; then
-	tar -C models --strip-components 1 -xvzf /mnt/c/linux/sttPackages/Models_Starterpack.tar.gz  || fatalerror "Failure during extraction of models"
-fi
-[ ! -d models/NL ] && fatalerror "Something went wrong: models were not installed."
-
-if [ ! -e models/Patch1 ]; then
-	tar -C models --strip-components 1 -xvzf /mnt/c/linux/sttPackages/Models_Patch1.tar.gz  || fatalerror "Failure during extraction of models"
-fi
-
-# Correct hardcoded paths in existing configuration files:
 for model in "$@"; do
     if [ -d "contrib/$model" ]; then
         if [ "$NODOWNLOAD" != "1" ] && [ -e "contrib/$model/configure_download.sh" ]; then
