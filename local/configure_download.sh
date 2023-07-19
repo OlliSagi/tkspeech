b1d62b6b6483fe58fca765a5a4ea73a8e6446ece
OlliSagi <ollisagittarius@gmail.com>
2023-07-19 13:26:44 +0200

    Update configure_download.sh

--- local/configure_download.sh
+++ local/configure_download.sh
@@ -5,20 +5,18 @@ if [ $sourced -eq 0 ]; then
 fi
 
 #Set model directory
-if [ ! -d models/NL ] && [ -z "$modelpack" ]; then
-    if [ -d models ]; then
-        modelpack=$(realpath models)
-    else
-        while [ $return_value -eq 0 ] && ! readlink -f $modelpack; do
-            modelpack=$(dialog --stdout --title "Models not found" --inputbox "Enter location to download & store models, do not use ~ " 0 0 "models")
-            return_value=$?
-        done
-        [ ! $return_value -eq 0 ] && fatalerror "Models not downloaded. Cancelling"
-    fi
+if [ -d models ] && [ -z "$modelpack" ]; then
+    modelpack=$(realpath models)
+elif [ ! -d models/NL ] && [ -z "$modelpack" ]; then
+    while [ $return_value -eq 0 ] && ! readlink -f $modelpack; do
+        modelpack=$(dialog --stdout --title "Models not found" --inputbox "Enter location to download & store models, do not use ~ " 0 0 "models")
+        return_value=$?
+    done
+    [ ! $return_value -eq 0 ] && fatalerror "Models not downloaded. Cancelling"
 fi
-mkdir -p $modelpack || fatalerror "Model base directory $modelpack does not exist and unable to create"
+[ -z "$modelpack" ] && fatalerror "Model base directory is empty"
+mkdir -p "$modelpack" || fatalerror "Model base directory $modelpack does not exist and unable to create"
 modelpack=$(realpath $modelpack)
-
 #link to the model pack directory from the kaldi_nl root
 if [ ! -e models ]; then
     ln -s -f $modelpack models
@@ -32,13 +30,26 @@ if [ ! -e models/Models ]; then
 fi
 
 # get models
+#
+if [ ! -d models/NL ]; then
+	tar -C models --strip-components 1 -xvzf /home/download/Models_Starterpack.tar.gz  || fatalerror "Failure during extraction of models"
+fi
+[ ! -d models/NL ] && fatalerror "Something went wrong: models were not installed."
+
+if [ ! -e models/Patch1 ]; then
+	tar -C models --strip-components 1 -xvzf /home/download/Models_Patch1.tar.gz  || fatalerror "Failure during extraction of models"
+fi
+
+# Correct hardcoded paths in existing configuration files:
+find -name "*.conf" | xargs sed -i "s|/home/laurensw/Documents/Models|$modelpack|g"
+
 for model in "$@"; do
     if [ -d "contrib/$model" ]; then
-        if [ "$NODOWNLOAD" != "1" ] && [ -e "contrib/$model/configure_download.sh" ]; then
-            . "contrib/$model/configure_download.sh"
+        if [ -e "contrib/$model/configure_download.sh" ]; then
+            source "contrib/$model/configure_download.sh"
         fi
-        for f in "contrib/$model/decode"*.sh; do
-            [ -e "$f" ] && [ ! -e "$(basename "$f")" ] && ln -s "$f" "$(basename "$f")"
+        for f in contrib/$model/decode*.sh; do
+            ln -s $f $(basename $f)
         done
     else
         echo "Specified model ($model) not found, expected a directory $root/contrib/$model/">&2
