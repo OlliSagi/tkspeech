932e561158ae20595bb810a60dc80aab2ca5878b
OlliSagi <ollisagittarius@gmail.com>
2023-07-19 12:55:08 +0200

    Update configure_download.sh

--- local/configure_download.sh
+++ local/configure_download.sh
@@ -5,24 +5,19 @@ if [ $sourced -eq 0 ]; then
 fi
 
 #Set model directory
-if [ "$NODOWNLOAD" = "1" ]; then
-    [ -z "$modelpack" ] && fatalerror "\$modelpack must be set if \$NODOWNLOAD is set"
-elif [ -d models ] && [ -z "$modelpack" ]; then
-    modelpack=$(realpath models)
-elif [ ! -d models/NL ] && [ -z "$modelpack" ]; then
-    while [ "$return_value" -eq 0 ] && ! readlink -f "$modelpack"; do
-        modelpack=$(dialog --stdout --title "Models not found" --inputbox "Enter location to download & store models, do not use ~ " 0 0 "models")
-        return_value=$?
-    done
-    [ ! "$return_value" -eq 0 ] && fatalerror "Models not downloaded. Cancelling"
+if [ ! -d models/NL ] && [ -z "$modelpack" ]; then
+    if [ -d models ]; then
+        modelpack=$(realpath models)
+    else
+        while [ $return_value -eq 0 ] && ! readlink -f $modelpack; do
+            modelpack=$(dialog --stdout --title "Models not found" --inputbox "Enter location to download & store models, do not use ~ " 0 0 "models")
+            return_value=$?
+        done
+        [ ! $return_value -eq 0 ] && fatalerror "Models not downloaded. Cancelling"
+    fi
 fi
-[ -z "$modelpack" ] && fatalerror "Model base directory is empty"
-mkdir -p "$modelpack" || fatalerror "Model base directory $modelpack does not exist and unable to create"
-modelpack="$(realpath "$modelpack")"
-#link to the model pack directory from the kaldi_nl root
-if [ ! -e models ]; then
-    ln -s -f "$modelpack" models
-fi
+mkdir -p $modelpack || fatalerror "Model base directory $modelpack does not exist and unable to create"
+modelpack=$(realpath $modelpack)
 
 #we need this very ugly patch,
 #creating a symlink back to itself,
