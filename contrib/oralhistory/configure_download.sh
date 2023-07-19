b0bd6b9b8d04ef3d8622bcebd4fd4fd68ab69e24
OlliSagi <ollisagittarius@gmail.com>
2023-07-19 11:19:50 +0200

    Update configure_download.sh

--- contrib/oralhistory/configure_download.sh
+++ contrib/oralhistory/configure_download.sh
@@ -5,10 +5,10 @@ if [ $sourced -eq 0 ]; then
 fi
 
 if [ ! -e models/Lang_OH ]; then
-    if [ ! -e oral_history_models.tar.gz ]; then
-        wget https://applejack.science.ru.nl/downloads/oral_history_models.tar.gz || fatalerror "Unable to download oral history models from applejack.science.ru.nl!"
-    fi
-    tar -C models --strip-components 1 -xvzf oral_history_models.tar.gz || fatalerror "Failure during extraction of models"
+    #if [ ! -e oral_history_models.tar.gz ]; then
+    #    wget oral_history_models.tar.gz [Add url for download if file is not on storage already, see knl repository for details] || fatalerror "Unable to download oral history models"
+    #fi
+     tar -C models --strip-components 1 -xvzf /home/olli/oral_history_models.tar.gz || fatalerror "Failure during extraction of models"
 
     rm oral_history_models.tar.gz
 
