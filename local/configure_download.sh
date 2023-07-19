a31a894a6f2a8598ead703ee9e7cf16470d596dc
OlliSagi <ollisagittarius@gmail.com>
2023-07-19 13:03:03 +0200

    Update configure_download.sh

--- local/configure_download.sh
+++ local/configure_download.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 (return 0 2>/dev/null) && sourced=1 || sourced=0
 if [ $sourced -eq 0 ]; then
     echo "this script should not be run directly but through configure.sh in the kaldi_nl root directory">&2
